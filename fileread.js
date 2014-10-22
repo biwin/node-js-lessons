44a9d9bcb2afd003be5764c159b5b6ae75d458da
biwin <biwin007@gmail.com>
2014-10-22 22:43:17 +0530

    asynchronous file read

---
+++ fileread.js
@@ -0,0 +1,11 @@
+/**
+ * Created by payload on 10/22/2014.
+ */
+//asynchronus fileread
+var fs = require("fs");
+console.log("Starting to read!!");
+fs.readFile("sample.txt", function(error, data){
+   console.log("the content of the file is:" + data)
+    console.log("Done reading the file")
+});
+console.log("Execute me without waiting please..!!")
