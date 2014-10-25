08284dc9b70ce6eb1be0b9044d68faee5a661d44
biwin <biwin007@gmail.com>
2014-10-25 13:07:35 +0530

    typo "file"

--- file read and write/write sync.js
+++ file read and write/write sync.js
@@ -4,5 +4,5 @@
 
 var fs = require("fs");
 console.log('Starting to write to a file!!');
-fs.writeFileSync("written_synchronously", "hi there, we wrote the fiel synchronously.")
+fs.writeFileSync("written_synchronously.txt", "hi there, we wrote the file synchronously.")
 console.log("Finished with writing the file.")
