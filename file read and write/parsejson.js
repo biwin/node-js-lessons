075e5fc5347f8cae9e3e1730be4d62784a2ea995
biwin <biwin007@gmail.com>
2014-10-25 13:03:03 +0530

    parse JSON file

--- file read and write/parsejson.js
+++ file read and write/parsejson.js
@@ -5,3 +5,7 @@ var fs = require("fs");
 console.log("Starting here!! Synchronously!!");
 var content = fs.readFileSync("package.json");
 console.log("the contents are : " + content );
+var configuration = JSON.parse(content);
+console.log("Configuration:", configuration);
+console.log("Name: ", configuration.name);
+// parse just the field 'name' from the package.json fle.
