f17ca7a40f59c3a6424931fee721b7ec385e3b51
biwin <biwin007@gmail.com>
2014-10-29 13:44:37 +0530

    specify the pages

--- webserver/server.js
+++ webserver/server.js
@@ -2,13 +2,23 @@
  * Created by payload on 10/27/2014.
  */
 var http = require("http");
+var fs = require("fs");
 console.log("Starting the server!");
 var host = "127.0.0.2";
 var port = 1337;
 var server = http.createServer(function(request, response){
     console.log("received request:" + request.url);
-    response.writeHead(200, {"Content-type":"text/plain"});
-    response.end("Hello Dude!!");
+
+    fs.readFile("./public" + request.url, function(error, data){
+       if (error){
+           response.writeHead(404, {"Content-type":"text/plain"});
+           response.end("Page not found dude!");
+       } else {
+           response.writeHead(200, {"Content-type":"text/html"});
+            response.end(data);
+       }
+    });
+
 });
 server.listen(port, host, function(){
    console.log("Listening" + host + ":" + port);
